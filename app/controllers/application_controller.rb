339441b1e7fe78d2f3189fea5acaeba3a7bacd7a
Thierry Fonquernie <thierry.fonquernie@gmail.com>
2022-02-14 12:00:00 +0100

    stupid coaching done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
+# app/controller/questions_controller.rb
 class ApplicationController < ActionController::Base
+  def ask
+  end
+
+  def anwser
+    @answer = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+  end
 end
